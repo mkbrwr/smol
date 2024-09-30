932560e5eb25547158adbf403cc99604a8e92fb4
Mykhailo Tymchyshyn <mykhailo.tymchyshyn@gmail.com>
2024-09-30 06:23:02 +0300

    collision detection

--- Game/engine.swift
+++ Game/engine.swift
@@ -28,7 +28,6 @@ struct Pixel {
     }
 
     init(argb: UInt32) {
-        //SEGGER_RTT_WriteString(0, "Pixel init")
         red = UInt8(truncatingIfNeeded: (argb & 0x00_ff0000) >> 16)
         green = UInt8(truncatingIfNeeded: (argb & 0x00_00ff00) >> 8)
         blue = UInt8(truncatingIfNeeded: (argb & 0x00_0000ff))
@@ -59,6 +58,12 @@ struct Size {
 struct Rectangle {
     let origin: Point
     let size: Size
+
+    func collides(with other: Rectangle) -> Bool {
+        let isHorizontalOverlap = origin.x < other.origin.x + other.size.width && origin.x + size.width > other.origin.x
+        let isVerticalOverlap = origin.y < other.origin.y + other.size.height && origin.y + size.height > other.origin.y
+        return isHorizontalOverlap && isVerticalOverlap
+    }
 }
 
 enum Sprite {
@@ -168,14 +173,27 @@ struct Vector {
 }
 
 class Entity {
+    var id = UInt32.random(in: 0x0..<0xffffffff)
     let sprite: Sprite
     var position: Point
     var direction: Vector
+    var body: Rectangle {
+        Rectangle(origin: position, size: sprite.size)
+    }
 
     init(sprite: Sprite, position: Point, direction: Vector) {
         self.sprite = sprite
         self.position = position
         self.direction = direction
+    }
+
+    func collides(with other: Entity) -> Bool {
+        body.collides(with: other.body)
+    }
+
+    func reverseDirection() {
+        direction.x *= -1
+        direction.y *= -1
     }
 }
 
@@ -184,9 +202,9 @@ final class SwiftEngine {
         screen: Screen(width: 240, height: 320, backgroundColor: Pixel(argb: 0xff00_0000)))
 
     var entities: [Entity] = [
-        Entity(sprite: Sprite.swiftLogo, position: Point(x: 10, y: 40), direction: Vector(x: 2, y: 2)),
-        Entity(sprite: Sprite.swiftLogo, position: Point(x: 100, y: 40), direction: Vector(x: -2, y: 2)),
-        Entity(sprite: Sprite.swiftLogo, position: Point(x: 10, y: 140), direction: Vector(x: 2, y: -2)),
+        Entity(sprite: Sprite.swiftLogo, position: Point(x: 1, y: 1), direction: Vector(x: 2, y: 2)),
+        Entity(sprite: Sprite.swiftLogo, position: Point(x: 80, y: 80), direction: Vector(x: -2, y: 2)),
+        Entity(sprite: Sprite.swiftLogo, position: Point(x: 160, y: 160), direction: Vector(x: 2, y: -2)),
         //Entity(sprite: Sprite.swiftLogo, position: Point(x: 10, y: 40), direction: Vector(x: 3, y: 3)),
         //Entity(sprite: Sprite.swiftLogo, position: Point(x: 100, y: 40), direction: Vector(x: -3, y: 3)),
         //Entity(sprite: Sprite.swiftLogo, position: Point(x: 10, y: 140), direction: Vector(x: 3, y: -3)),
@@ -197,6 +215,12 @@ final class SwiftEngine {
     func onUpdate() {
         renderer.screen.clear()
         for entity in entities {
+            for otherEntity in entities where otherEntity.id != entity.id {
+                if entity.collides(with: otherEntity) {
+                    entity.reverseDirection()
+                    otherEntity.reverseDirection()
+                }
+            }
             if entity.position.x + entity.sprite.size.width >= 240 || entity.position.x <= 0 {
             entity.direction.x = -entity.direction.x
             }
